679fd68d6a59ddb34a62e72703d4d870f8a27035
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2023-06-28 16:21:59 +0200

    CORS :100:

--- HTTP_REST_service/app/main.py
+++ HTTP_REST_service/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .db import engine, get_db
 from .routers import aquapod, user, auth
@@ -7,7 +9,19 @@ from . import models
 
 models.Base.metadata.create_all(bind=engine)
 
+origins = [
+    "http://localhost:5173",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
